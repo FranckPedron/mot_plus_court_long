116317754dcf11c0c18f90e3f018d227770440b6
FranckPedron <franckyp1+rapports_git@gmail.com>
2024-05-09 10:58:04 +0200

    ajout du tri alphabétique

--- main.py
+++ main.py
@@ -9,4 +9,17 @@ def get_min_and_max_words(sentence):
     return min_word, max_word
 
 
-get_min_and_max_words("Un chasseur sachant chasser sait chasser sans son chien")
+def get_min_and_max_words_sorted(sentence):
+    words = sentence.split(" ")
+    sorted_words = sorted(words)
+    min_word = min(sorted_words, key=len)
+    max_word = max(sorted_words, key=len)
+
+    print("Mot le plus petit:", min_word)
+    print("Mot le plus long:", max_word)
+
+    return min_word, max_word
+
+
+get_min_and_max_words("Un qq sachant chasser sait chasser sans son chien")
+get_min_and_max_words_sorted("Un qq sachant chasser sait chasser sans son chien")
